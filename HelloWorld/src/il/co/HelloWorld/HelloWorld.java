e9bf0dbf3fdd9f9a9140c6e5cd836f1f9d2eed2e
artmeleshko <artem.meleshko@gmail.com>
2016-08-16 13:45:21 +0300

    Third Report

--- HelloWorld/src/il/co/HelloWorld/HelloWorld.java
+++ HelloWorld/src/il/co/HelloWorld/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Hello Bentley!");
 		System.out.println("First change!");
+		System.out.println("second change!");
+		System.out.println("second change!");
 		
 
 	}
